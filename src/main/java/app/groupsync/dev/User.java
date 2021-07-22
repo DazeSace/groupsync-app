ed1a7a9490a28ef3a0c18bffb59ace50a6886841
dazesace <dazemcblaze@gmail.com>
2021-07-22 23:05:58 +0200

    add method for user + getter n setter

--- src/main/java/app/groupsync/dev/User.java
+++ src/main/java/app/groupsync/dev/User.java
@@ -19,4 +19,37 @@ public class User extends PanacheEntity {
     private String firstname;
     private String lastname;
     private LocalDateTime birthday;
+
+    @Transactional
+    public static User userAdd(String firstname, String lastname, LocalDateTime birthday) {
+        var user = new User();
+        user.setFirstname(firstname);
+        user.setLastname(lastname);
+        user.setBirthday(birthday);
+        return user;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public LocalDateTime getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDateTime birthday) {
+        this.birthday = birthday;
+    }
 }
