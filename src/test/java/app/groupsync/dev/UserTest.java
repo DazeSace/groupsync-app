522ccaa1d37b7d3223b91f8b45f056bc05d1228b
dazesace <dazemcblaze@gmail.com>
2021-07-22 23:44:02 +0200

    tests running

--- src/test/java/app/groupsync/dev/UserTest.java
+++ src/test/java/app/groupsync/dev/UserTest.java
@@ -29,4 +29,26 @@ class UserTest {
         assertEquals(1, users.size());
     }
 
+    @Test
+    void userHasFirstname() {
+        var user = User.userAdd("Jimmy", "Choo", LocalDateTime.now());
+        List<User> users = User.listAll();
+        assertEquals("Jimmy", users.get(0).getFirstname());
+    }
+
+    @Test
+    void userHasLastname() {
+        var user = User.userAdd("Jimmy", "Choo", LocalDateTime.now());
+        List<User> users = User.listAll();
+        assertEquals("Choo", users.get(0).getLastname());
+    }
+
+    @Test
+    void userHasBirthdate() {
+        var birthday = LocalDateTime.now();
+        var user = User.userAdd("Jimmy", "Choo", birthday);
+        List<User> users = User.listAll();
+        assertEquals(birthday, users.get(0).getBirthday());
+    }
+
 }
